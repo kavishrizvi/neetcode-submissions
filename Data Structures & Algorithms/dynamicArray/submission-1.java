fb29368c79ca04260e728dba793c4c7a61e8f6c9
Kavish Rizvi <kavish.r@icloud.com>
2026-04-26 22:51:45 +0530

    Add: dynamicArray - submission-1

---
+++ Data Structures & Algorithms/dynamicArray/submission-1.java
@@ -0,0 +1,70 @@
+class DynamicArray {
+
+    private Integer[] array;
+
+    public DynamicArray(int capacity) {
+        if (capacity <= 0) {
+            throw new IllegalArgumentException("capacity must be greater than 0");
+        }
+        this.array = new Integer[capacity];
+    }
+
+    public int get(int i) {
+        return array[i];
+    }
+
+    public void set(int i, int n) {
+        array[i] = n;
+    }
+
+    public void pushback(int n) {
+        if (isFull()) {
+            resize();
+        }
+        Integer lastIndex = 0;
+        while (array[lastIndex] != null) {
+            lastIndex++;
+        }
+        push(lastIndex, n);
+    }
+
+    public int popback() {
+        Integer lastIndex = array.length - 1;
+        while (array[lastIndex] == null) {
+            lastIndex--;
+        }
+        Integer popped = array[lastIndex];
+        array = pop(lastIndex);
+        return popped;
+    }
+
+    public void resize() {
+        array = Arrays.copyOf(array, getCapacity() * 2);
+    }
+
+    public int getSize() {
+        int size = 0;
+        for (Integer i : array) {
+            if (i != null) {
+                size++;
+            }
+        }
+        return size;//return number of elements
+    }
+
+    public int getCapacity() {
+        return array.length;
+    }
+
+    public boolean isFull() {
+        return array[array.length - 1] != null;
+    }
+    private Integer[] pop(Integer index) {
+        array[index] = null;
+        return array;
+    }
+    private void push(Integer index, Integer n) {
+        array[index] = n;
+    }
+
+}
